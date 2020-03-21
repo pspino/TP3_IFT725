d133412da6d0054fa7a72fc9d86baac2ca525e56
Victor Fan <victor.fan@usherbrooke.ca>
2020-03-21 18:39:49 -0400

    basic ift725net

--- prog/src/models/IFT725Net.py
+++ prog/src/models/IFT725Net.py
@@ -10,7 +10,10 @@ Other: Suggestions are welcome
 
 import torch.nn as nn
 from models.CNNBaseModel import CNNBaseModel
+from models.CNNBlocks import ConvolutionalBlock
+from models.CNNBlocks import DenseBlock
 from models.CNNBlocks import ResidualBlock
+from models.CNNBlocks import BottleneckBlock
 
 '''
 TODO
@@ -27,7 +30,6 @@ Ajouter du code ici pour faire fonctionner le réseau IFT725Net.  Le réseau est
 
 '''
 
-
 class IFT725Net(CNNBaseModel):
     """
     Class that mix up several sort of layers to create an original network
@@ -41,8 +43,24 @@ class IFT725Net(CNNBaseModel):
             network's weights.
         """
         super(IFT725Net, self).__init__()
+        self.in_channels = 64
 
-
+        self.layer1 = ConvolutionalBlock(3, 64)
+        self.layer2 = ConvolutionalBlock(64, 128)
+        self.layer3 = DenseBlock(128, 256, stride=2)
+        self.layer4 = ResidualBlock(256, 512, stride=2)
+        self.layer5 = BottleneckBlock(512, 64, 512, stride=2)
+        self.linear = nn.Linear(512 * 4 * 4, num_classes)
+    
+    def forward(self, x):
+        output = self.layer1(x)
+        output = self.layer2(output)
+        output = self.layer3(output)
+        output = self.layer4(output)
+        output = self.layer5(output)
+        output = output.view(output.size(0), -1)  # reshape feature maps
+        output = self.linear(output)
+        return output
 
 '''
 FIN DE VOTRE CODE
